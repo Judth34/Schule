8208250c3a70455a985c88863f4c754ef84ff485
Marcel Judth <marjudth@gmail.com>
2018-09-27 21:58:02 +0200

    update

--- 5.AHIFS/DBI/Program/001_BLOB_Browser/001_BLOB_Browser/MainWindow.xaml.cs
+++ 5.AHIFS/DBI/Program/001_BLOB_Browser/001_BLOB_Browser/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,28 @@ namespace _001_BLOB_Browser
     /// </summary>
     public partial class MainWindow : Window
     {
+        OracleConnection connection;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnFileDialog_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = @"C:\Users\Marcel Judth\Documents\GitHub\Schule\5.AHIFS\NVS\Programs\001_ImageFilter\001_ImageFilter\bin\Debug\Data";
+            openFileDialog.Title = "Browse images";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Image files (*.jpg, *.bmp, *.png) | *.jpg; *.bmp; *.png";
+            var result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = openFileDialog.FileName;
+                string connectionStringOracle = "Data Source=192.168.128.152/ora11g;PERSIST SECURITY INFO=True;User Id = d4a06; Password = d4a;";
+                connection = new OracleConnection(connectionStringOracle);
+                connection.Open();
+            }
         }
     }
 }
