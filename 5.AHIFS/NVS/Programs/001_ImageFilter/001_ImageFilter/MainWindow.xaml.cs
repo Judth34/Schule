606128dbe9f20beabed82fbf32e2de0a44619fbc
Marcel Judth <marjudth@gmail.com>
2018-10-11 09:24:22 +0200

    finished

--- 5.AHIFS/NVS/Programs/001_ImageFilter/001_ImageFilter/MainWindow.xaml.cs
+++ 5.AHIFS/NVS/Programs/001_ImageFilter/001_ImageFilter/MainWindow.xaml.cs
@@ -21,7 +21,8 @@ namespace _001_ImageFilter
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] colors = { "Red", "Green", "Blue" }; 
+        string[] colors = { "Red", "Green", "Blue" };
+        BitmapImage originalImage;
 
         public MainWindow()
         {
@@ -42,42 +43,75 @@ namespace _001_ImageFilter
             if (result == true)
             {
                 string filename = openFileDialog.FileName;
-                BitmapImage image = new BitmapImage();
-                image.BeginInit();
-                image.UriSource = new Uri(filename);
-                image.EndInit();
-                int bytesperpixel = image.Format.BitsPerPixel / 8;
-                int stride = image.PixelWidth * bytesperpixel;
-                byte[] pixels = new byte[stride * image.PixelHeight];
-                image.CopyPixels(pixels, stride, 0);
-
-                
-
-                byte[] r = new byte[pixels.Length];
-                byte[] g = new byte[pixels.Length];
-                byte[] b = new byte[pixels.Length];
-
-                for (int i = 0; i < pixels.Length; i = i + bytesperpixel)
-                {
-                    r[i] = pixels[i];
-                    g[i + 1] = pixels[i + 1];
-                    b[i + 2] = pixels[i + 2];
-                    r[i + 3] = pixels[i + 3];
-                    g[i + 3] = pixels[i + 3];
-                    b[i + 3] = pixels[i + 3];
-                }
-
-                WriteableBitmap image2 = new WriteableBitmap((int)image.PixelWidth, (int)image.PixelHeight, image.DpiX, image.DpiY, image.Format, null);
-                var rect = new Int32Rect(0, 0, (int)image.PixelWidth, (int)image.PixelHeight);
-                image2.WritePixels(rect, b, stride, 0);
-                this.imgOriginal.Source = image2;
-                System.Diagnostics.Debug.WriteLine("finished!");
+                originalImage = new BitmapImage();
+                originalImage.BeginInit();
+                originalImage.UriSource = new Uri(filename);
+                originalImage.EndInit();
+                this.imgOriginal.Source = originalImage;
             }
         }
 
         private void cmbFilters_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
+        }
+
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            int bytesperpixel = originalImage.Format.BitsPerPixel / 8;
+            int stride = originalImage.PixelWidth * bytesperpixel;
+            byte[] pixels = new byte[stride * originalImage.PixelHeight];
+            originalImage.CopyPixels(pixels, stride, 0);
+
+
+
+            byte[] r = new byte[pixels.Length];
+            byte[] g = new byte[pixels.Length];
+            byte[] b = new byte[pixels.Length];
+
+            for (int i = 0; i < pixels.Length; i = i + bytesperpixel)
+            {
+                b[i] = pixels[i];
+                g[i + 1] = pixels[i + 1];
+                r[i + 2] = pixels[i + 2];
+                r[i + 3] = pixels[i + 3];
+                g[i + 3] = pixels[i + 3];
+                b[i + 3] = pixels[i + 3];
+            }
+
+            WriteableBitmap filteredImage = new WriteableBitmap((int)originalImage.PixelWidth, (int)originalImage.PixelHeight, originalImage.DpiX, originalImage.DpiY, originalImage.Format, null);
+            var rect = new Int32Rect(0, 0, (int)originalImage.PixelWidth, (int)originalImage.PixelHeight);
+            if ((bool)rBNormal.IsChecked)
+            {
+                filteredImage.WritePixels(rect, pixels, stride, 0);
+            }
+            else
+            {
+                if ((bool)rBRed.IsChecked)
+                {
+                    filteredImage.WritePixels(rect, r, stride, 0);
+                }
+                else
+                {
+                    if ((bool)rBGreen.IsChecked)
+                    {
+                        filteredImage.WritePixels(rect, g, stride, 0);
+                    }
+                    else
+                    {
+                        if ((bool)rBBlue.IsChecked)
+                        {
+                            filteredImage.WritePixels(rect, b, stride, 0);
+                        }
+                    }
+                }
+            }
+
+
+
+
+            this.imgFiltered.Source = filteredImage;
+            System.Diagnostics.Debug.WriteLine("finished!");
         }
     }
 }
